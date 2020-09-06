723601557567e67dbeecdceea66b7fc4a7fa234a
tpgmartin <tpgmartin@gmail.com>
2020-09-06 13:41:24 +0100

    Refactor script

--- scripts/generate_heatmap_from_occlusion_images.py
+++ scripts/generate_heatmap_from_occlusion_images.py
@@ -1,9 +1,25 @@
 from glob import glob
 import matplotlib.pyplot as plt
-import numpy as np
 import random
-from skimage import draw
 from skimage import io
+
+def init_mask(width, height):
+
+    img_mask = []
+    for y in range(height):
+        img_mask.append([])
+        for _ in range(width):
+            img_mask[y].append([0,0,0])
+    
+    return img_mask
+
+def save_image(image_to_save, filepath, width, height, dpi):
+
+    fig, ax = plt.subplots(figsize=((width/dpi),(height/dpi)), dpi=dpi)
+    ax.imshow(image_to_save)
+    ax.axis('off')
+    plt.tight_layout(pad=0)
+    plt.savefig(filepath)
 
 def main():
 
@@ -18,64 +34,41 @@ def main():
     x_coords = [random.randint(0,(WIDTH-MASK_SIZE)) for _ in range(MAX_MASKS)]
     y_coords = [random.randint(0,(HEIGHT-MASK_SIZE)) for _ in range(MAX_MASKS)]
 
-    img_mask = []
-    for y in range(HEIGHT):
-        img_mask.append([])
-        for _ in range(WIDTH):
-            img_mask[y].append([0,0,0])
+    # Create blank image mask
+    img_mask = init_mask(WIDTH, HEIGHT)
     
+    # Apply occlusion masks
+    max_intensity = 0
     for x_coord, y_coord in zip(x_coords, y_coords):
         for x in range(x_coord, x_coord+MASK_SIZE):
             for y in range(y_coord, y_coord+MASK_SIZE):
+                
                 img_mask[y][x][0] += 1
 
-    max_ = 0
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            if img_mask[y][x][0] > max_:
-                max_ = img_mask[y][x][0]
+                if img_mask[y][x][0] > max_intensity:
+                    max_intensity = img_mask[y][x][0]
 
-    # Save as mask on black background
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-                img_mask[y][x][0] /= max_
-
-    fig, ax = plt.subplots(figsize=((WIDTH/DPI),(HEIGHT/DPI)), dpi=DPI)
-    ax.imshow(img_mask)
-        
-    ax.axis('off')
-    plt.tight_layout(pad=0)
-    plt.savefig(f'./test_heatmap.JPEG')
-
-    # Save image cropped to mask
     img_cropped_to_mask = IMG.copy()
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            img_cropped_to_mask[y][x][0] *= img_mask[y][x][0]
-            img_cropped_to_mask[y][x][1] *= img_mask[y][x][0]
-            img_cropped_to_mask[y][x][2] *= img_mask[y][x][0]
+                # Find normalised mask on black background
+                img_mask[y][x][0] /= max_intensity
 
-    fig, ax = plt.subplots(figsize=((WIDTH/DPI),(HEIGHT/DPI)), dpi=DPI)
-    ax.imshow(img_cropped_to_mask)
-    ax.axis('off')
-    plt.tight_layout(pad=0)
-    plt.savefig(f'./image_cropped_to_mask.JPEG')
+                # Crop input image and channel intensities to masks
+                img_cropped_to_mask[y][x][0] *= img_mask[y][x][0]
+                img_cropped_to_mask[y][x][1] *= img_mask[y][x][0]
+                img_cropped_to_mask[y][x][2] *= img_mask[y][x][0]
 
-    # Save image with mask overlaid
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            IMG[y][x][0] = (1-img_mask[y][x][0])*IMG[y][x][0] + 255*img_mask[y][x][0]
-            IMG[y][x][1] = (1-img_mask[y][x][0])*IMG[y][x][1]
-            IMG[y][x][2] = (1-img_mask[y][x][0])*IMG[y][x][2]
-            # IMG[y][x][1] *= 0
-            # IMG[y][x][2] *= 0
+                # Overlay masks on original image
+                IMG[y][x][0] = (1-img_mask[y][x][0])*IMG[y][x][0] + 255*img_mask[y][x][0]
+                IMG[y][x][1] = (1-img_mask[y][x][0])*IMG[y][x][1]
+                IMG[y][x][2] = (1-img_mask[y][x][0])*IMG[y][x][2]
 
-    fig, ax = plt.subplots(figsize=((WIDTH/DPI),(HEIGHT/DPI)), dpi=DPI)
-    ax.imshow(IMG)
-    ax.axis('off')
-    plt.tight_layout(pad=0)
-    plt.savefig(f'./image_with_mask.JPEG')
+    save_image(img_mask, f'./test_heatmap.JPEG', WIDTH, HEIGHT, DPI)
+    save_image(img_cropped_to_mask, f'./image_cropped_to_mask.JPEG', WIDTH, HEIGHT, DPI)
+    save_image(IMG, f'./image_with_mask.JPEG', WIDTH, HEIGHT, DPI)
 
 if __name__ == '__main__':
 
-    main(IMG)
+    main()
+    # main(IMG)
