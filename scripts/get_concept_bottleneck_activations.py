c33eb39785cde3f6ed0df5bdabe50e01797c0c00
tpgmartin <tpgmartin@gmail.com>
2020-09-19 11:18:59 +0100

    WIP find CAVs for superpixels

--- scripts/get_concept_bottleneck_activations.py
+++ scripts/get_concept_bottleneck_activations.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pathlib import Path
 from PIL import Image
 from skimage import io
+import sklearn.cluster as cluster
 import sys
 from tcav import utils
 import tcav.model as model
@@ -52,8 +53,12 @@ def extract_patch(image, DPI=72):
                 y_coords.append(y)
 
     # Get x,y-coords of image segment
-    x_min, x_max = np.min(x_coords), np.max(x_coords)
-    y_min, y_max = np.min(y_coords), np.max(y_coords)
+    try:
+        x_min, x_max = np.min(x_coords), np.max(x_coords)
+        y_min, y_max = np.min(y_coords), np.max(y_coords)
+    except ValueError:
+        # No image segments that boost prediction probability - so exit here
+        return
 
     # Crop input image to dimensions of image segment
     cropped_image = []
@@ -65,15 +70,17 @@ def extract_patch(image, DPI=72):
             cropped_image[y-y_min][x-x_min].append(IMG[y][x][1])
             cropped_image[y-y_min][x-x_min].append(IMG[y][x][2])
 
-    filepath = f"./patches/{IMAGE_CAT}/{IMAGE_NO}/mask_dim_{MASK_DIM}/{IMAGE_NO}_patch"
+    # Change name "patch"
+    filepath = f"./patches/{IMAGE_CAT}/{IMAGE_NO}/mask_dim_{MASK_DIM}/{IMAGE_NO}_superpixels"
     os.makedirs(filepath, exist_ok=True)
 
     # Save patch resized to original image dimensions
     cropped_image = Image.fromarray((np.array(cropped_image)).astype(np.uint8))
     image_resized = np.array(cropped_image.resize([IMG.shape[1], IMG.shape[0]], Image.BICUBIC))
-    Image.fromarray(image_resized).save(f"{filepath}/{IMAGE_NO}_patch.png", format='PNG')
+    # Change name "patch"
+    Image.fromarray(image_resized).save(f"{filepath}/{IMAGE_NO}_superpixels.png", format='PNG')
 
-    return image_resized, filepath
+    return image_resized, cropped_image, filepath
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
@@ -97,17 +104,82 @@ if __name__ == '__main__':
     # 2. Resize each patch to original input image size
 
     # ^ For these steps just want to call somthing like `_extract_patch`
+    # TODO: Need to indentify all seperate segments of given image e.g. in cases where image segments are not contiguous
 
     # 3. Find activation for each patch - this is pretty much what I've already done, but would be applied to patches from previous steps only 
     # 4. Apply clustering - just standard k-means
     # 5. Remove outliers from clustering technique - this creates bottleneck dictionary for given bottleneck layer
 
-    # images = glob('./net_occlusion_heatmaps_delta_prob/**/**/**/**/*')
-    # for image in images:
-    img_path = './net_occlusion_heatmaps_delta_prob/n09229709/n09229709_47343/mask_dim_100/n09229709_47343_image_cropped_to_mask/n09229709_47343_image_cropped_to_mask.JPEG'
-    patch, filepath = extract_patch(img_path)
+    # 6. Discover CAVs
+    filepaths = []
+    images = []
+    patches = []
+    bn_activations = []
+    # labels = []
+    # costs = []
 
-    args = parse_arguments(sys.argv[1:])
-    args.source_dir = filepath
-    bn_activations = get_patch_activations(args)
+    # "bubble" images only
+    images = glob('./net_occlusion_heatmaps_delta_prob/n09229709/**/**/*_image_cropped_to_mask/*')[:3]
+    for image in images:
+    # img_path = './net_occlusion_heatmaps_delta_prob/n09229709/n09229709_47343/mask_dim_100/n09229709_47343_image_cropped_to_mask/n09229709_47343_image_cropped_to_mask.JPEG'
+        print('------------------------------')
+        print(image)
+        print('------------------------------')
+        superpixel, patch, filepath = extract_patch(image)
+        if filepath:
+            filepaths.append(filepath)
+            args = parse_arguments(sys.argv[1:])
+            args.source_dir = filepath
+            # Get activation for superpixel
+            bn_activations.append(get_patch_activations(args)[0])
+
+    # Save to CSV
+
+    # Cluster activations
+    # * Perform clustering on bottleneck activations using given method
+    # * cluster method: use k-means
+    # * (Check format of bn_dic['label'])
+    # * For given label in bn_activations, if have sufficient number of images (at least 20)
+    # * Check if concept images meet 1 of 3 conditons:
+    #     - Concept found in more than half of images of given label
+    #     - Concept found in more than quarter of images of given label AND number of supporting 
+    # images more quarter of all images
+    #     - Concept found in more than 10% of images of given label AND number of supporting 
+    # images more half of all images
+    # * If true then for each concept reutrn images, partcher, and image numbers
+
+    centers = None
+    n_clusters = 3
+    km = cluster.KMeans(n_clusters)
+    d = km.fit(bn_activations)
+    centers = km.cluster_centers_
+    d = np.linalg.norm(np.expand_dims(bn_activations, 1) - np.expand_dims(centers, 0), ord=2, axis=-1)
+    labels, costs = np.argmin(d, -1), np.min(d, -1)
+
+    # label concepts by target label and concept number
+    # if highly_common_concept or cond2 or cond3:
+    #         concept_number += 1
+    #         concept = '{}_concept{}'.format(self.target_class, concept_number)
+    #         bn_dic['concepts'].append(concept)
+    #         bn_dic[concept] = {
+    #             'images': self.dataset[concept_idxs],
+    #             'patches': self.patches[concept_idxs],
+    #             'image_numbers': self.image_numbers[concept_idxs]
+    #         }
+    #         bn_dic[concept + '_center'] = centers[i]
+
+    concepts = []
+    for l in range(len(labels)):
+        concept = {}
+        concept['concept'] = l
+        concept['center'] = centers[l]
+        idxs = [idx for idx, x in enumerate(labels) if x == l]
+        concept['images'] = [filepaths[idx] for idx in idxs]
+        concepts.append(concept)
+
+    print('+++++++++++++++++++++++++++++++++++++')
+    print(concepts)
+    print('+++++++++++++++++++++++++++++++++++++')
+    # ^ Save labels and centers
+
 
