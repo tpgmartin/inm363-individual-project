0211c43d3ccd0b44adb175065802a0dc83e0509c
tpgmartin <tpgmartin@gmail.com>
2020-12-23 04:44:53 +0000

    Find bottleneck activations and similarity of image subset

--- scripts/get_image_cosine_similarities_different_labels.py
+++ scripts/get_image_cosine_similarities_different_labels.py
@@ -8,44 +8,60 @@ from concept_discovery import ConceptDiscovery
 
 if __name__ == '__main__':
 
-	labels = glob('./acts/**/')
+	# Label pairings to check
+	# * Bookshop and restaurant, cinema and restaurant
+	# * Cab and jeep, ambulance and jeep
+	# * Ant and mantis, damselfly and mantis
+	# * Bubble and balloon
+	# * Lipstick and lotion
+	# * Volleyball and basketball
+	label_pairings = [
+		['./acts/bookshop/','./acts/restaurant/'],
+		['./acts/cinema/','./acts/restaurant/'],
+		['./acts/cab/','./acts/jeep/'],
+		['./acts/ambulance/','./acts/jeep/'],
+		['./acts/ant/','./acts/mantis/'],
+		['./acts/damselfly/','./acts/mantis/'],
+		['./acts/bubble/','./acts/balloon/'],
+		['./acts/lipstick/','./acts/lotion/'],
+		['./acts/volleyball/','./acts/basketball/']
+	]
 
-	for i in range(len(labels)-1):
-		for j in range(i+1,len(labels)):
+	for labels in label_pairings:
 
-			labels_1 = []
-			labels_2 = []
-			imgs_1 = []
-			imgs_2 = []
-			cos_sims = []
+		labels_1 = []
+		labels_2 = []
+		imgs_1 = []
+		imgs_2 = []
+		cos_sims = []
 
-			if len(glob(f'{labels[i]}*')) != len(glob(f'{labels[j]}*')):
-				continue
+		if len(glob(f'{labels[0]}*')) != len(glob(f'{labels[1]}*')):
+			continue
 
-			for img_1 in glob(f'{labels[i]}*'):
-				for img_2 in glob(f'{labels[j]}*'):
+		for img_1 in glob(f'{labels[0]}*'):
+			for img_2 in glob(f'{labels[1]}*'):
 
-					label_1 = labels[i].split('/')[-2]
-					label_2 = labels[j].split('/')[-2]
+				label_1 = labels[0].split('/')[-2]
+				label_2 = labels[1].split('/')[-2]
 
-					labels_1.append(label_1)
-					labels_2.append(label_2)
-					imgs_1.append(img_1)
-					imgs_2.append(img_2)
+				labels_1.append(label_1)
+				labels_2.append(label_2)
+				imgs_1.append(img_1)
+				imgs_2.append(img_2)
 
-					image_1_acts = np.load(img_1).squeeze()
-					image_2_acts = np.load(img_2).squeeze()
-					cos_sims.append(cosine_similarity(image_1_acts, image_2_acts))
-		
-			df = pd.DataFrame({
-				'labels_1': labels_1,
-				'labels_2': labels_2,
-				'image_1': imgs_1,
-				'image_2': imgs_2,
-				'cosine_sim': cos_sims
-			})
+				image_1_acts = np.load(img_1).squeeze()
+				image_2_acts = np.load(img_2).squeeze()
+				cos_sims.append(cosine_similarity(image_1_acts, image_2_acts))
+	
+		df = pd.DataFrame({
+			'labels_1': labels_1,
+			'labels_2': labels_2,
+			'image_1': imgs_1,
+			'image_2': imgs_2,
+			'cosine_sim': cos_sims
+		})
 
-			df.sort_values(by='cosine_sim', inplace=True, ascending=False)
+		df.sort_values(by='cosine_sim', inplace=True, ascending=False)
 
-			df.to_csv(f"./cosine_similarities/different_labels_only/{labels[i].split('/')[-2]}_{labels[j].split('/')[-2]}_cosine_similarities.csv", index=False)
-			print(f"Found cosine similarities for {labels[i].split('/')[-2]} & {labels[j].split('/')[-2]}")
+		df.to_csv(f"./cosine_similarities/different_labels_only/{labels[0].split('/')[-2]}_{labels[1].split('/')[-2]}_cosine_similarities.csv", index=False)
+		print(f"Found cosine similarities for {labels[0].split('/')[-2]} & {labels[1].split('/')[-2]}")
