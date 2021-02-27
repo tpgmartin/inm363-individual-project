935bcc5ec1e368a16626d9ec03073c65008d53f7
tpgmartin <tpgmartin@gmail.com>
2021-02-27 11:05:35 +0000

    Find concept activation plots for top N concepts

--- scripts/dim_red_check.py
+++ scripts/dim_red_check.py
@@ -4,10 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA, SparsePCA
 
-chart_type = 'all_concepts'
-label = 'ambulance'
+chart_type = 'top_10'
+label = 'lipstick'
 layer = 'mixed8'
 concepts = np.unique([x.split('_')[2] for x in glob(f'./acts/{label}/*_{layer}') if 'concept' in x]).tolist()
+
+# Read from ACE result file top N concepts by TCAV score
+if 'top' in chart_type:
+    start = 4
+    stop = start + int(chart_type.split('_')[-1])
+
+    with open(f'../ACE/ACE/results_summaries/{layer}_{label}_ace_results.txt') as f:
+        lines = f.readlines()
+
+    top_concepts = [line.split(':')[1].split('_')[-1] for line in lines[start:stop]]
+
+    concepts = [concept for concept in concepts if concept in top_concepts]
 
 for concept in concepts:
     image_acts = np.array([np.load(acts).squeeze() for acts in glob(f'./acts/{label}/acts_{label}_{concept}_*_{layer}')])
@@ -23,7 +35,8 @@ for concept in concepts:
         plt.scatter(image_acts_embedded[:,0], image_acts_embedded[:,1], label=f'{concept}')
 
 if chart_type != 'all_concepts':
-    plt.legend(loc='upper right')
+    concept_labels = ['Concept ' + concept.split('concept')[-1] for concept in np.unique(top_concepts)]
+    plt.legend(concept_labels, loc='upper right')
 
 plt.xlabel('Component 1')
 plt.ylabel('Component 2')
