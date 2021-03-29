2f18271d771ba0dabbce0a6b6bea2ef80e007089
tpgmartin <tpgmartin@gmail.com>
2021-03-29 23:20:10 +0100

    WIP find ImageNet hierarchy

--- scripts/nltk_wordnet_hierarchy.py
+++ scripts/nltk_wordnet_hierarchy.py
@@ -1,3 +1,5 @@
+import pattern
+# from pattern import en
 import nltk
 from nltk.corpus import wordnet
 
@@ -9,11 +11,40 @@ def get_synonyms(synset):
 
 # e.g. dog.n.01 corresponds to the noun "dog"
 # synset = wordnet.synsets('cinema')
-synset = wordnet.synset('cinema.n.02')
-print(synset.lemmas())
-# synset = wordnet.synsets('dog')
-# print(synset[0].name())
-# for words in synset:
-#     print(words.name())
-# print(synset[0].lemmas()[0].name())
-# print(get_synonyms(dog))
+# synset = wordnet.synset('cinema.n.02')
+# print(synset.lemmas())
+
+# Get synonyms
+def get_synonyms(synset):
+    synonyms = []
+    for l in synset.lemmas():
+        synonyms.append(l.name())
+    return synonyms
+
+# print(set(get_synonyms(wordnet.synset('dog.n.01'))))
+print(wordnet.synsets('bookshop'))
+print(set(get_synonyms(wordnet.synset('bookshop.n.01'))))
+
+# Get hypernyms
+# For bookshop should get
+# shop
+# v
+# place of business, establishment
+# v
+# establishment
+# v
+# structure
+# v artifact
+def get_hypernyms(synset):
+    hypernyms = set()
+    for hypernym in synset.hypernyms():
+        hypernyms |= set(get_hypernyms(hypernym))
+    return hypernyms | set(synset.hypernyms())
+bookshop = wordnet.synset('bookshop.n.01')
+print(get_hypernyms(bookshop))
+
+bookshop = wordnet.synset('bookshop.n.01')
+print(bookshop.hypernyms())
+# print(bookshop.holonyms())
+
+print(wordnet.synset('bookshop.n.01').holonyms())
