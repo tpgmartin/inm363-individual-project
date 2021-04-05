2680c4687f8207522daddbf7ee1a0ba5972a6cdd
tpgmartin <tpgmartin@gmail.com>
2021-04-05 13:13:13 +0100

    Refactor

--- scripts/check_num_activations_for_concepts.py
+++ scripts/check_num_activations_for_concepts.py
@@ -1,46 +1,58 @@
+import argparse
 from glob import glob
+import sys
 
-label = 'school_bus'
-short_label = label.split('_')[0]
-layer = 'mixed8'
+def main(args):
+    label = args.label
+    layer = args.layer
+    short_label = label.split('_')[0]
 
-concepts = glob(f'../ACE/ACE/concepts/{layer}_{label}_concept*/*')
-concepts = [concept for concept in concepts if '.png' in concept]
-acts = glob(f'./acts/{short_label}/*{layer}')
+    concepts = glob(f'../ACE/ACE/concepts/{layer}_{label}_concept*/*')
+    concepts = [concept for concept in concepts if '.png' in concept]
+    acts = glob(f'./acts/{short_label}/*{layer}')
 
-# mixed8:restaurant_concept19:0.7025±0.07980444849756184,2.942195744994734e-07
-# mixed8:restaurant_concept22:0.67875±0.11132020256898564,9.68045324475897e-07
-# mixed8:restaurant_concept7:0.675±0.1414213562373095,0.00036661620810570633
-# mixed8:restaurant_concept8:0.6487499999999999±0.13192682630913244,2.158266286688261e-05
-# mixed8:restaurant_concept10:0.62625±0.13907619314605935,0.0007136937473238669
-with open(f'../ACE/ACE/results_summaries/{layer}_{label}_ace_results.txt') as f:
-    lines = f.readlines()
+    with open(f'../ACE/ACE/results_summaries/{layer}_{label}_ace_results.txt') as f:
+        lines = f.readlines()
 
-top_concepts = [line.split(':')[1].split('_')[-1] for line in lines[4:14]]
+    top_concepts = [line.split(':')[1].split('_')[-1] for line in lines[4:14]]
 
-# ../ACE/ACE/concepts/mixed8_restaurant_concept2/016_13/016_13.png
-images_for_concepts = {}
-for concept_img in concepts:
-    concept = concept_img.split('/')[4].split('_')[-1]
-    if concept in images_for_concepts:
-        images_for_concepts[concept] += 1
-    else:
-        images_for_concepts[concept] = 1
+    images_for_concepts = {}
+    for concept_img in concepts:
+        concept = concept_img.split('/')[4].split('_')[-1]
+        if concept in images_for_concepts:
+            images_for_concepts[concept] += 1
+        else:
+            images_for_concepts[concept] = 1
 
-acts_for_concepts = {}
-for act in acts:
-    concept = act.split('/')[-1].split('_')[2]
-    if concept in acts_for_concepts:
-        acts_for_concepts[concept] += 1
-    else:
-        acts_for_concepts[concept] = 1
+    acts_for_concepts = {}
+    for act in acts:
+        concept = act.split('/')[-1].split('_')[2]
+        if concept in acts_for_concepts:
+            acts_for_concepts[concept] += 1
+        else:
+            acts_for_concepts[concept] = 1
 
-for concept in top_concepts:
-    try:
-        print(concept)
-        if images_for_concepts[concept] != acts_for_concepts[concept]:
-            print('Missing acts for', concept)
-        print('Total super-pixel images:', images_for_concepts[concept])
-        print('Total activation files:', acts_for_concepts[concept])
-    except KeyError:
-        pass
+    for concept in top_concepts:
+        try:
+            print(concept)
+            if images_for_concepts[concept] != acts_for_concepts[concept]:
+                print('Missing acts for', concept)
+            print('Total super-pixel images:', images_for_concepts[concept])
+            print('Total activation files:', acts_for_concepts[concept])
+        except KeyError:
+            pass
+
+def parse_arguments(argv):
+		parser = argparse.ArgumentParser()
+		parser.add_argument('--label', type=str,
+			help='The name of the target class to be interpreted', default='ambulance')
+		parser.add_argument('--layer', type=str,
+				help='Names of the target layers of the network (comma separated)',
+												default='mixed8')
+		return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+
+    args = parse_arguments(sys.argv[1:])
+    main(args)
