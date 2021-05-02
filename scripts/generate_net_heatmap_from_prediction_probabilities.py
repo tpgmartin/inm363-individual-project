b61b5a88229db5094e98258777e6ff6965da454c
tpgmartin <tpgmartin@gmail.com>
2021-05-02 21:51:48 +0100

    For heatmaps, run one-tailed test only

--- scripts/generate_net_heatmap_from_prediction_probabilities.py
+++ scripts/generate_net_heatmap_from_prediction_probabilities.py
@@ -45,33 +45,23 @@ def main(f, label, mask_size, z_value):
 
     # bookshop_image_n02871525_10490_occluded_image_predictions
     df = pd.read_csv(f'./occluded_image_predictions/mask_dim_{MASK_SIZE}/{label}_image_{f}_occluded_image_predictions.csv')
+    print(df)
     # prediction_probability_delta = df['true_label_prediction_probability_delta'].values
 
     prob_mean = df['true_label_prediction_probability_delta'].mean()
     prob_std = df['true_label_prediction_probability_delta'].std()
     df['standardised_prediction_prob'] = df['true_label_prediction_probability_delta'].apply(lambda p: (p-prob_mean)/prob_std)
 
-    df = df[(df['standardised_prediction_prob']>z_value)|(df['standardised_prediction_prob']<(-1*z_value))]
-
-    # Negative means does contribute to image prediction so should be red
-    # Postiive should be blue
-
-    # Find occlusion masks for remaining rows of df
-    # directory,mask_dim,filename,true_label,true_label_predictions,predicted_label,prediction_probability,true_label_prediction_probability_delta
-
-    # for y in range(HEIGHT):
-    #     for x in range(WIDTH):
-    #         net_intensity = int(false_heatmap[y][x][0]) - int(true_heatmap[y][x][0])
-
-    #         all_net_intensities.append(net_intensity)
-    #         img_mask[y][x][0] = net_intensity
+    df = df[(df['standardised_prediction_prob']<(-1*z_value))]
+    df = df.iloc[:10,:]
+    print(df)
 
     for _, row in df.iterrows():
         filename = row['filename'][:-4] + 'csv'
         if row['standardised_prediction_prob'] < 0:
             pixel_intensity = 1
-        elif row['standardised_prediction_prob'] > 0:
-            pixel_intensity = -1
+        # elif row['standardised_prediction_prob'] > 0:
+        #     pixel_intensity = -1
             
         mask_coords = pd.read_csv(filename)
 
@@ -83,32 +73,22 @@ def main(f, label, mask_size, z_value):
                     updated_pixel_intensity = img_mask[row['y_min']][row['x_min']][0]
 
     pos_vals = []
-    neg_vals = []
+    # neg_vals = []
     for y in range(HEIGHT):
         for x in range(WIDTH):
             pixel_intensity = img_mask[y][x][0]
 
             if pixel_intensity > 0:
                 pos_vals.append(pixel_intensity)
-            elif pixel_intensity < 0:
-                neg_vals.append(abs(pixel_intensity))
+            # elif pixel_intensity < 0:
+            #     neg_vals.append(abs(pixel_intensity))
 
-    print('Total pixels:', HEIGHT*WIDTH)
-    print('No. positive:', len(pos_vals))
-    print('No. negative:', len(neg_vals))
     if len(pos_vals) != 0:
         ub_pos_val = np.max(pos_vals)
         lb_pos_val = np.min(pos_vals)
     else:
         ub_pos_val = 0
         lb_pos_val = 0
-
-    if len(neg_vals) != 0:
-        ub_neg_val = np.max(neg_vals)
-        lb_neg_val = np.min(neg_vals)
-    else:
-        ub_neg_val = 0
-        lb_neg_val = 0
 
     img_cropped_to_mask = IMG.copy()
     for y in range(HEIGHT):
@@ -120,14 +100,6 @@ def main(f, label, mask_size, z_value):
                     intensity = (intensity - lb_pos_val)/(ub_pos_val - lb_pos_val)
                     img_mask[y][x][0] = intensity
                     img_mask[y][x][2] = 0
-                except ZeroDivisionError:
-                    img_mask[y][x][0] = 0
-                    img_mask[y][x][2] = 0
-            elif intensity < 0:
-                try:
-                    intensity = (abs(intensity) - lb_neg_val)/(ub_neg_val - lb_neg_val)
-                    img_mask[y][x][0] = 0
-                    img_mask[y][x][2] = intensity
                 except ZeroDivisionError:
                     img_mask[y][x][0] = 0
                     img_mask[y][x][2] = 0
@@ -149,25 +121,39 @@ def main(f, label, mask_size, z_value):
                 IMG[y][x][1] = (1-img_mask[y][x][0]-img_mask[y][x][2])*IMG[y][x][1]
                 IMG[y][x][2] = (1-img_mask[y][x][0]-img_mask[y][x][2])*IMG[y][x][2] + 255*img_mask[y][x][2]
             except ValueError:
+                print(img_mask[y][x][0])
+                print(IMG[y][x][0])
+                print(IMG[y][x][1])
+                print(IMG[y][x][2])
                 IMG[y][x][0] = (1-img_mask[y][x][0])*IMG[y][x][0] + 255*img_mask[y][x][0]
                 IMG[y][x][1] = (1-img_mask[y][x][0])*IMG[y][x][1]
                 IMG[y][x][2] = (1-img_mask[y][x][0])*IMG[y][x][2]
 
-    save_image(img_mask, 'net_heatmap', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
-    save_image(img_cropped_to_mask, 'image_cropped_to_mask', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
-    save_image(IMG, 'image_with_mask', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
+    save_image(img_mask, f'net_heatmap_z_value_{z_value}', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
+    save_image(img_cropped_to_mask, f'image_cropped_to_mask_z_value_{z_value}', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
+    save_image(IMG, f'image_with_mask_z_value_{z_value}', WIDTH, HEIGHT, DPI, f, MASK_SIZE)
     plt.clf()
 
 if __name__ == '__main__':
 
     # mapping = map_images_to_labels()
+    mask_size = 100
 
-    occlusion_heatmaps = [f.split('/')[-1] for f in glob('./occluded_image_predictions/**/*')]
-    occlusion_heatmaps = [f for f in occlusion_heatmaps if '_' in f]
-    existing_heatmaps = [f.split('/')[-1] for f in glob('./net_occlusion_heatmaps/**/*')]
+    # occlusion_heatmaps = [f.split('/')[-1] for f in glob('./occluded_image_predictions/**/*')]
+    # occlusion_heatmaps = [f for f in occlusion_heatmaps if '_' in f]
+    # existing_heatmaps = [f.split('/')[-1] for f in glob('./net_occlusion_heatmaps/**/*')]
     # heatmaps = list(set(occlusion_heatmaps) - set(existing_heatmaps))
+    # heatmaps = [f for f in occlusion_heatmaps if 'n09229709_28418' in f]
+    # for f in heatmaps:
+        # print(f)
+    f = 'jeep_image_n03594945_7578_occluded_image_predictions.csv'
 
-    heatmaps = [f for f in occlusion_heatmaps if 'n09229709_28418' in f]
-    for f in heatmaps:
-        print(f)
-        main(('_').join(f.split('_')[2:4]), f.split('_')[0], 1.96)
+    # one-tailed test
+    # p-value | z-score
+    # 0.1586552594 | 1
+    # 0.05    | 1.644854
+    # 0.01    | 2.644854
+    print(f)
+    print(('_').join(f.split('_')[2:4]))
+    print(f.split('_')[0])
+    main(('_').join(f.split('_')[2:4]), f.split('_')[0], mask_size, 1)
