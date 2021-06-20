1d3660947d7d5314794000532f01b108e052f409
tpgmartin <tpgmartin@gmail.com>
2021-06-21 00:48:10 +0100

    Get overlap of concept segments with occluded image elements

--- scripts/find_overlap_with_occlusion_mask.py
+++ scripts/find_overlap_with_occlusion_mask.py
@@ -1,6 +1,7 @@
 from glob import glob
 from multiprocessing import dummy as multiprocessing
 import numpy as np
+import pandas as pd
 from PIL import Image
 import tensorflow as tf
 
@@ -16,50 +17,84 @@ import tensorflow as tf
 
 layer = 'mixed4c'
 target_class = 'cinema'
+name_lookup = pd.read_csv(f'../ACE/ACE/concepts/images/{target_class}/image_name_lookup.csv')
 all_concepts = len(glob(f'../ACE/ACE/masks/{layer}_{target_class}_{target_class}_concept*_masks/'))
 
-# load image segment masks by concept and image
-img_segment_masks = {}
-# TODO: check why class name duplicated
-for concept_no in range(all_concepts):
 
-    concept_no += 1
-    img_segment_masks[concept_no] = {}
-    for mask_filepath in glob(f'../ACE/ACE/masks/{layer}_{target_class}_{target_class}_concept{concept_no}_masks/*_39.npy'):
-        mask_filename = mask_filepath.split('/')[-1]
+images = []
+concept_nos = []
+img_filenames = []
+overlaps = []
+for idx, row in name_lookup.iterrows():
 
-        mask_ = np.load(mask_filepath).squeeze()
+    image_name = int(row['image_name'].split('.')[0]) # e.g. 1 - 40
+    image_filename = row['image_filename'] # e.g. ./ImageNet/ILSVRC2012_img_train/n03032252/img_sample/cinema/n03032252_17822.JPEG
+    image = image_filename.split('/')[-1][:-5] # e.g. n03032252_17822
 
-        img_segment_masks[concept_no][mask_filename] = np.load(mask_filepath).squeeze()
+    mask_no = image_name - 1
+    class_no = image.split('_')[0]
+    image_no = image.split('_')[-1]
 
 
-# # Load occlusion results
-occlusion_img_filename = './net_occlusion_heatmaps_delta_prob/n03032252/n03032252_10737/mask_dim_100/n03032252_10737_image_mask/n03032252_10737_image_mask.npy'
-occlusion_img_mask = np.load(occlusion_img_filename).squeeze()
+    print('image:', image)
 
-jaccard_sim_by_mask = {}
-max_concept_no = None
-max_img_filename = None
-max_jaccard = 0
-for concept_no in range(all_concepts):
+    # load image segment masks by concept and image
+    img_segment_masks = {}
+    # TODO: check why class name duplicated
+    for concept_no in range(all_concepts):
 
-    concept_no += 1
-    img_segment_masks_by_concept = img_segment_masks[concept_no]
+        concept_no += 1
+        img_segment_masks[concept_no] = {}
+        for mask_filepath in glob(f'../ACE/ACE/masks/{layer}_{target_class}_{target_class}_concept{concept_no}_masks/*_{mask_no}.npy'):
+            mask_filename = mask_filepath.split('/')[-1]
 
-    if len(img_segment_masks_by_concept) > 0:
-        for img_filename, mask in img_segment_masks_by_concept.items():
+            mask_ = np.load(mask_filepath).squeeze()
 
-            # mask = (...)
-            if np.mean(mask) > 0.001:
-                jaccard = np.sum(occlusion_img_mask * mask) / np.sum((occlusion_img_mask + mask) > 0)
-                
-                if jaccard > max_jaccard:
-                    max_concept_no = concept_no
-                    max_img_filename = img_filename
-                    max_jaccard = jaccard
-                # jaccard_sim_by_mask[concept_no][img_filename] = jaccard
+            img_segment_masks[concept_no][mask_filename] = np.load(mask_filepath).squeeze()
 
-print(concept_no)
-print(img_filename)
-print(jaccard)
-print('----------')
+
+    # # Load occlusion results
+    occlusion_img_filename = f'./net_occlusion_heatmaps_delta_prob/{class_no}/{image}/mask_dim_50/{image}_image_mask/{image}_image_mask.npy'
+    occlusion_img_mask = np.load(occlusion_img_filename).squeeze()
+
+    for concept_no in range(all_concepts):
+
+        overlap_sim_by_mask = {}
+        max_concept_no = None
+        max_img_filename = None
+        max_overlap = 0
+
+        concept_no += 1
+        img_segment_masks_by_concept = img_segment_masks[concept_no]
+
+        if len(img_segment_masks_by_concept) > 0:
+            for img_filename, mask in img_segment_masks_by_concept.items():
+
+                # mask = (...)
+                if np.mean(mask) > 0.001:
+                    # Update this, as occlusion mask is point of reference only 
+                    # care about fraction of mask contained in occluion image
+                    # TODO: Need to check if numerator larger than 0 as per denominator
+                    # Or for numerator: could take sum of two masks and check total number of elements == 2
+                    # overlap = np.sum(occlusion_img_mask * mask) / np.sum(occlusion_img_mask > 0)
+                    overlap = np.sum((mask + occlusion_img_mask) > 1) / np.sum(occlusion_img_mask > 0)
+                    
+                    if overlap > max_overlap:
+                        max_concept_no = concept_no
+                        max_img_filename = img_filename
+                        max_overlap = overlap
+                    # overlap_sim_by_mask[concept_no][img_filename] = overlap
+        
+        images.append(image)
+        concept_nos.append(max_concept_no)
+        img_filenames.append(max_img_filename)
+        overlaps.append(max_overlap)
+
+df = pd.DataFrame({
+    'images': images,
+    'concept_nos': concept_nos,
+    'img_filenames': img_filenames,
+    'overlaps': overlaps
+})
+
+df.to_csv(f'{layer}_{target_class}_concept_image_segment_overlap.csv', index=False)
